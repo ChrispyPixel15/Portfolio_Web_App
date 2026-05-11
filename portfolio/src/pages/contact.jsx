f1b300f1a190b2bffe1d8ef6f3f55d57de89ceff
NightLocke15 <christinefrances.holt@gmail.com>
2026-05-11 11:41:43 +0200

    socials links

--- portfolio/src/pages/contact.jsx
+++ portfolio/src/pages/contact.jsx
@@ -6,23 +6,23 @@ function Contact() {
         <div className="contact-content">
             <h1>Contact</h1>
             <div className="icons-contact-holder">
-                <a className="social-icon-contact" href="">
+                <a className="social-icon-contact" href="https://github.com/ChrispyPixel15">
                     <GithubLogoIcon size={32} className="icon-contact" weight="fill" />
                     <p>GitHub</p>
                 </a>
-                <a className="social-icon-contact" href="">
+                <a className="social-icon-contact" href="https://www.instagram.com/chris.designed/">
                     <InstagramLogoIcon size={32} className="icon-contact" weight="fill" />
                     <p>Instagram</p>
                 </a>
-                <a className="social-icon-contact" href="">
+                <a className="social-icon-contact" href="https://www.youtube.com/@chris.designed">
                     <YoutubeLogoIcon  size={32} className="icon-contact" weight="fill" />
                     <p>YouTube</p>
                 </a>
-                <a className="social-icon-contact" href="">
+                <a className="social-icon-contact" href="https://www.linkedin.com/in/christine-holt-8821252b6">
                     <LinkedinLogoIcon  size={32} className="icon-contact" weight="fill" />
                     <p>LinkedIn</p>
                 </a>
-                <a className="social-icon-contact" href="">
+                <a className="social-icon-contact" href="mailto:chris.designed@gmail.com">
                     <EnvelopeSimpleIcon  size={32} className="icon-contact" weight="fill" />
                     <p>Email</p>
                 </a>              
--- portfolio/src/pages/home.jsx
+++ portfolio/src/pages/home.jsx
@@ -7,19 +7,19 @@ function Home() {
             <h1>Christine</h1>
             <p>Designing and Developing Apps, Web Apps and Websites.</p>
             <div className="icons-holder">
-                <a className="social-icon" href="">
+                <a className="social-icon" href="https://github.com/ChrispyPixel15">
                     <GithubLogoIcon size={32} className="icon" weight="fill" color="#50586C" />
                 </a>
-                <a className="social-icon" href="">
+                <a className="social-icon" href="https://www.instagram.com/chris.designed/">
                     <InstagramLogoIcon size={32} className="icon" weight="fill" color="#50586C" />
                 </a>
-                <a className="social-icon" href="">
+                <a className="social-icon" href="https://www.youtube.com/@chris.designed">
                     <YoutubeLogoIcon  size={32} className="icon" weight="fill" color="#50586C" />
                 </a>
-                <a className="social-icon" href="">
+                <a className="social-icon" href="https://www.linkedin.com/in/christine-holt-8821252b6">
                     <LinkedinLogoIcon  size={32} className="icon" weight="fill" color="#50586C" />
                 </a>
-                <a className="social-icon" href="">
+                <a className="social-icon" href="mailto:chris.designed@gmail.com">
                     <EnvelopeSimpleIcon  size={32} className="icon" weight="fill" color="#50586C" />
                 </a>                
             </div>
